9196415d47bcb6e13bf1097873bf1fc3a17c3c49
lh <2549852401@qq.com>
2021-02-16 18:31:28 +0800

    extract github config from code to properties

---
+++ src/main/java/com/bjpowernode/springboot/controller/AuthorizeController.java
@@ -0,0 +1,39 @@
+package com.bjpowernode.springboot.controller;
+
+import com.bjpowernode.springboot.dto.AccessTokenDTO;
+import com.bjpowernode.springboot.dto.GithubUser;
+import com.bjpowernode.springboot.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+    @RequestMapping("callback")
+    public String callback(@RequestParam(name ="code")String code,
+                           @RequestParam(name ="state")String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+        return "index";
+    }
+}
